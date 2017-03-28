92305771c5e8396ff4a03e09a976d6483fa8a784
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-03-28 19:44:24 +0200

    ExoElectroaimant

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
 	
 
-	// la fonction crere point prend en parametre n le nombre de point voulue
+	// la fonction createPoint prend en parametre n le nombre de point voulue
 	function createPoint(n){
 		// je recupere le width et height dans le css de #jeu
 		var width = $('#jeu').css("width");
@@ -14,7 +14,7 @@ $( document ).ready(function() {
 		height = parseInt(height);
 
 		
-		//je fais une boucle jusquau nombre de point voulu n
+		//je fais une boucle jusqu'au nombre de point voulu n
 		for(var i = 0; i < n ; i++){
 			//avec un chiffre aleatoir entre 0 et 1 en multipliant par width
 			//j'obtient une valeur aleatoire entre 0 et width (500)
@@ -24,7 +24,7 @@ $( document ).ready(function() {
 			console.log(xpos);
 			console.log(ypos);
 
-			//je creer a la volé une span de classs debris
+			//je crée à la volé un span de classs debris
 			//j'insere les valeur trouver plus haut dans les propriete css
 			var deb = $('<span class="debris"></span>').css({ "top": ypos , "left": xpos });
 			$('#jeu').append(deb);
